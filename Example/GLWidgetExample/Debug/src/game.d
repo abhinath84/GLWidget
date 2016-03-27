ec0c6f212aba5e41c0cc414b59720f08e5145c2a
Abhishek Nath <abhi.nath@outlook.com>
2016-03-28 02:09:08 +0530

    Add emaple project

---
+++ Example/GLWidgetExample/Debug/src/game.d
@@ -0,0 +1,12 @@
+src/game.o: ../src/game.cpp ../src/game.h \
+ /home/abhi/Documents/C++/GLWidgetExample/lib/glwidget.h \
+ /home/abhi/Documents/C++/GLWidgetExample/lib/point.h \
+ /home/abhi/Documents/C++/GLWidgetExample/lib/color.h
+
+../src/game.h:
+
+/home/abhi/Documents/C++/GLWidgetExample/lib/glwidget.h:
+
+/home/abhi/Documents/C++/GLWidgetExample/lib/point.h:
+
+/home/abhi/Documents/C++/GLWidgetExample/lib/color.h:
